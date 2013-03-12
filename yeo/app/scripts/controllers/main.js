4b97a9844ba5c873a5e018e4c9d65bc123b8ff58
Josh Valdez <exxonvaldeez@gmail.com>
2013-03-12 01:58:56 -0700

    Added Socket Stream

---
+++ yeo/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lancetApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Testacular'
+    ];
+  }]);
